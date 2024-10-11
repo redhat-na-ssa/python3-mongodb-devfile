5b7aecda157b9c62483714e722d76b36c489eaa6
Rafael T. C. Soares <rafaelcba@gmail.com>
2024-10-11 15:39:24 -0500

    adding debug launch

---
+++ pymongo-fastapi-crud/pymongo_tutorial.mongodb.js
@@ -0,0 +1,40 @@
+/* global use, db */
+// MongoDB Playground
+// To disable this template go to Settings | MongoDB | Use Default Template For Playground.
+// Make sure you are connected to enable completions and to be able to run a playground.
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+// The result of the last command run in a playground is shown on the results panel.
+// By default the first 20 documents will be returned with a cursor.
+// Use 'console.log()' to print to the debug output.
+// For more documentation on playgrounds please refer to
+// https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
+
+// Select the database to use.
+use('pymong_tutorial');
+
+db.getCollectionNames()
+   
+db.books.insertMany(
+  [ 
+    {
+      _id: '001',
+      title: 'Openshift 101',
+      author: 'Red Hat',
+      synopsis: 'Learning Openshift'
+    },
+    {
+      _id: '002',
+      title: 'Python in Action',
+      author: 'Python',
+      synopsis: 'Learning python'
+    },
+    {
+      _id: '003',
+      title: 'Java in Action',
+      author: 'Sun',
+      synopsis: 'The Java programming language'
+    }
+  ]
+)
+
+db.books.find()
